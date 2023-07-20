3ca6290eb5e42abb7f00283ed9d7219c379f05af
Dora Kutsenko <kk.tasha@yandex.ru>
2023-07-20 15:54:36 +0300

    cors twi

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors({
-  origin: 'https://DoraAquadora.student.nomoredomains.xyz',
+  origin: true,
   credentials: true,
 }));
 
